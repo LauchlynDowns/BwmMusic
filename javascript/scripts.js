6cffc8d3764c75ee83b6f78b08238a95e70b5041
LauchlynDowns <49620443+LauchlynDowns@users.noreply.github.com>
2019-10-22 14:07:37 +0100

    Add files via upload
    done in college

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,54 @@
 
+$(document).ready(function () {
+    $("#facebooklink").hover(function () {
+        $(this).css("color", "#3b5998");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#instagramlink").hover(function () {
+        $(this).css("color", "#C13584");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#spotifylink").hover(function () {
+        $(this).css("color", "#1DB954");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#twitterlink").hover(function () {
+        $(this).css("color", "#1DA1F2");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#link1").hover(function () {
+        $(this).css("color", "white");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#link2").hover(function () {
+        $(this).css("color", "white");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#link3").hover(function () {
+        $(this).css("color", "white");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#link4").hover(function () {
+        $(this).css("color", "white");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#link5").hover(function () {
+        $(this).css("color", "white");
+    }, function () {
+        $(this).css("color", "#CCCCCC");
+    });
+    $("#linkitem1, #linkitem2, #linkitem3, #linkitem4").hover(function () {
+        $(this).css("border", "4px solid #606060");
+    }, function () {
+        $(this).css("border", "0");
+    });
+});
 
-function changebordercolor1() {
-document.getElementById("1").style.borderColor = "red";
-};
